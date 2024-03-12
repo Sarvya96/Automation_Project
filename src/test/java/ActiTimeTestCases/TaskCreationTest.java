ff0bb4fa61182fbd4728f58692f8215277f3ec1e
Sarvesh Tulangekar <159706622+Sarvya96@users.noreply.github.com>
2024-03-12 19:35:18 +0530

    TaskCreationTest.java
    Updated Test Case

--- src/test/java/ActiTimeTestCases/TaskCreationTest.java
+++ src/test/java/ActiTimeTestCases/TaskCreationTest.java
@@ -10,16 +10,19 @@ import BaseTestForAll.BaseTest;
 public class TaskCreationTest extends BaseTest{
 	
 	@Test
-	public void createTask()
+	public void createTask() throws InterruptedException
 	{
 		ActiTimeLoginPage atlp = new ActiTimeLoginPage();
 		String actMsg = atlp.enterName("Manager").enterpassword("manager")
 								.clickOnLoginBtn().clickOnTasksOption().clickOnCreateTask().selectCustomer().
-								selectProject().enterInTaskBox().enterInDeadlineBox()
-									.selectBillable().clickOnCreateTaskBtn().TaskCreationSuccessMsg();
+								selectProject().enterInTaskBox().
+									clickOnCreateTaskBtn().TaskCreationSuccessMsg();
+
+		//Validation Of Test Case
+		String customer = "\"Tata Consultancy Services\"";
+		String project = "\"Time Tracking Application\"";
 		
-		
-		String expMsg ="1 new task was added to the customer \"Tata Consultancy Services\", project \"Time Tracking Application\".";
+		String expMsg ="1 new task was added to the customer "+customer+", "+"project "+project+".";
 		System.out.println("Confirmation Msg :- "+ expMsg);
 		Assert.assertEquals(actMsg, expMsg,"Actual msg is not matchig with expected msg");
 		
