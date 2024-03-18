29da53af1d96040b4180bd8af897e8d3ec524219
Sarvesh Tulangekar <159706622+Sarvya96@users.noreply.github.com>
2024-03-18 19:44:57 +0530

    Final Commit

--- src/test/java/ActiTimeTestCases/ProjectDeletionTest.java
+++ src/test/java/ActiTimeTestCases/ProjectDeletionTest.java
@@ -16,13 +16,13 @@ public class ProjectDeletionTest extends BaseTest{
 		
 		String actMsg = atlp.enterName("Manager").enterpassword("manager").clickOnLoginBtn().
 						clickOnTasksOption().clickOnProjectBtn()
-						.clickOnProjectCheckBox().clickOnDeleteBtn()
+						.clickOnProjectName("Gaming Application").clickOnDeleteThisProjectBtn()
 						.clickOnDeleteProjectBtn().getProjectDeletionSuccessMsg();
 		
 		System.out.println("Confirmation Msg :- "+ actMsg);
 		
 		//Validation Of Test Case
-		String expMsg = "Selected projects have been successfully deleted.";
+		String expMsg = "Project has been successfully deleted.";
 		Assert.assertEquals(actMsg, expMsg,"Actual msg does not matched with expected msg");
 	}
 }
